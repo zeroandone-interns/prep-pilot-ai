bbc8c420c83344adae35d4e9384a1f2808e76508
Monah Haidar <mnhaidar01@gmail.com>
2025-08-18 10:58:08 +0300

    feat: implement TranscribeService for audio transcription functionality

---
+++ modules/shared/services/transcrible.py
@@ -0,0 +1,48 @@
+from datetime import time
+import json
+import boto3
+import urllib
+from extensions import get_logger
+
+
+class TranscribeService: 
+    def __init__(self):
+        self.transcribe_client = boto3.client('transcribe')
+        self.logger = get_logger()
+
+    def start_transcription(self, job_name, media_uri, media_format, language_code):
+        response = self.transcribe_client.start_transcription_job(
+            TranscriptionJobName=job_name,
+            Media={'MediaFileUri': media_uri},
+            MediaFormat=media_format,
+            LanguageCode=language_code
+        )
+        return response
+
+    def get_transcription(self, job_name):
+        max_tries = 60
+        while max_tries > 0:
+            max_tries -= 1
+            job = self.transcribe_client.get_transcription_job(TranscriptionJobName=job_name)
+            job_status = job['TranscriptionJob']['TranscriptionJobStatus']
+            if job_status in ['COMPLETED', 'FAILED']:
+                self.logger.info(f"Job {job_name} is {job_status}.")
+                if job_status == 'COMPLETED':
+                    response = urllib.request.urlopen(job['TranscriptionJob']['Transcript']['TranscriptFileUri'])
+                    data = json.loads(response.read())
+                    text = data['results']['transcripts'][0]['transcript']
+                    self.logger.info("========== below is output of speech-to-text ========================")
+                    self.logger.info(text)
+                    self.logger.info("=====================================================================")
+                return text
+            elif job_status == 'FAILED':
+                self.logger.error(f"Job {job_name} failed.")
+                return None
+            else:
+                self.logger.info(f"Waiting for {job_name}. Current status is {job_status}.")
+            time.sleep(10)
+
+
+
+    
+           
